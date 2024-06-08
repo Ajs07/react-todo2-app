328abe5f3447c7a24db730759cba89b2b1588365
Ajs07 <arnoldjulian24@gmail.com>
2024-06-09 00:57:26 +0700

    Develop toggle todo feature

---
+++ src/components/Todos.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import TodoItem from './TodoItem'
+
+// Menerima function toggleCompleted sebagai sebuah prop
+const Todos = ({ todos, toggleCompleted }) => {
+  return (
+    <div style={styles.container}>
+    {/* Gunakan method map di sini 
+      key={todo.id} seharusnya ditambahkan pada masing-masing elemen dalam sebuah array untuk memberikan identitas yang unik dan membantu React mendeteksi element mana yang telah di-render.*/}
+      {todos.map((todo) => {
+        return <TodoItem key={todo.id} todo={todo} toggleCompleted={toggleCompleted}/>
+      })}
+    </div>
+  )
+}
+
+// Menuliskan sebuah object berisikan property CSS langsung di dalam JSX
+const styles = {
+  container: {
+    width: '40%',
+    margin: '0 auto',
+  },
+}
+
+export default Todos
